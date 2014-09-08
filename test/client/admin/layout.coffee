3356702a55f74733dcf464d34e8ecdbc1f19bc39
Maxim Shekera <max.shekera@gmail.com>
2014-09-09 00:24:55 +0300

    folder for client testing

---
+++ test/client/admin/layout.coffee
@@ -0,0 +1,5 @@
+chai and chai.should() or require('chai').should()
+
+describe 'test', ->
+    it 'should pass', ->
+        (true).should.be.true
---
+++ test/client/admin/layout.js
@@ -0,0 +1,7 @@
+chai && chai.should() || require('chai').should();
+
+describe('test', function() {
+  return it('should pass', function() {
+    return true.should.be["true"];
+  });
+});
