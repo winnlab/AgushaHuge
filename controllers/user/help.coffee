a506fe612a6792a2f360418b223b5b36160cbec2
HydraOrc <hydra0@bigmir.net>
2014-10-07 20:24:05 +0300

    help faq page with search

--- controllers/user/help.coffee
+++ controllers/user/help.coffee
@@ -5,9 +5,11 @@ View = require '../../lib/view'
 tree = require '../../utils/tree'
 
 breadcrumbs = require '../../meta/breadcrumbs'
+faq = require '../../meta/faq'
 
 exports.index = (req, res) ->
 	data =
 		breadcrumbs: tree.findWithParents breadcrumbs, 'help'
+		faq: faq
 	
 	View.render 'user/help/index', res, data
--- public/js/app/user/modules/help/help.js
+++ public/js/app/user/modules/help/help.js
@@ -6,8 +6,90 @@ export default Controller.extend(
 			
         }
     }, {
+		variables: function() {
+			this.search_timeout = false;
+			this.faq_blocks = this.element.find('.faq_block');
+		},
+		
 		after_init: function(data) {
+			if(data) {
+				this.faq = data.faq;
+			} else {
+				this.faq = app.faq;
+			}
 			
-		}
+			this.faq_blocks.filter(':not(.active)').find('.text').hide();
+		},
+		
+		'#faq_search keyup': function(el, ev) {
+			var that = this;
+			
+			clearTimeout(this.search_timeout);
+			
+			this.search_timeout = setTimeout(function() {
+				that.search(el.val());
+			}, 300);
+		},
+		
+		search: function(val) {
+			val = escape(val).toLowerCase();
+			
+			var filtered =  _.filter(this.faq, function(faq) {
+				if(faq.title.toLowerCase().match(val) || faq.text.toLowerCase().match(val)) {
+					return true;
+				}
+				
+				return false;
+			});
+			
+			this.show_filtered(filtered);
+		},
+		
+		show_filtered: function(filtered) {
+			var i,
+				j,
+				found,
+				block,
+				flt,
+				func;
+			
+			for(i = this.faq_blocks.length; i--;) {
+				block = this.faq_blocks[i];
+				
+				found = false;
+				
+				for(j = filtered.length; j--;) {
+					flt = filtered[j];
+					
+					if(block.id == 'faq_block_' + flt.id) {
+						found = true;
+						break;
+					}
+				}
+				
+				if(!found) {
+					func = 'hide';
+				} else {
+					func = 'show';
+				}
+				
+				$(block)[func]();
+			}
+		},
+		
+		'.faq_block .title click': function(el) {
+			var faq_block = el.parent(),
+				text = el.next(),
+				classname = 'active',
+				func = 'slideDown';
+			
+			if(faq_block.hasClass(classname)) {
+				func = 'slideUp';
+			}
+			
+			text.stop(true, false)[func](300);
+			
+			faq_block.toggleClass(classname);
+		},
     }
 );
